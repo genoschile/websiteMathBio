066971b712664d3f3ee297216c7317a711945120
bnjmn <benja_e_e@hotmail.com>
2025-05-29 16:13:19 -0400

    mejoras

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [],
-
-  collections: {
-    members: {
-      // Aquí defines el formato de la colección con JSON
-      path: './src/content/members/*.json', // Ruta de tus archivos JSON
-    },
-    images: {
-        path: './src/content/images/*.json', // Ruta de tus archivos JSON
-        type: 'data'
-    },
-    project: {
-        path: './src/content/project/*.json', // Ruta de tus archivos JSON
-    },
-    publications: {
-        path: './src/content/publications/*.json', // Ruta de tus archivos JSON
-    },
-    talks: {
-        path: './src/content/talks/*.json', // Ruta de tus archivos JSON
-    }
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
