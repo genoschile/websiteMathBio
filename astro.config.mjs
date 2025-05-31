9c7fcc15c13b6af0fc9b763e92d2e6e2e886fd1e
Benjamín Ignacio Astudillo <bastudillo.alarcon@gmail.com>
2025-05-31 03:35:39 +0200

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,13 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [],
   site: "http://mathbio.genomas.cl",
-
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server',
+  output: "server",
   adapter: node({
     mode: "standalone",
   }),
 });
-
 
 /* HOST=0.0.0.0 PORT=4321 pm2 start dist/server/entry.mjs --name mathbio */
